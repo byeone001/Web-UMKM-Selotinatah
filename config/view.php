5c319f82725150274f31573dcd25c6944eec7fd6
Byeone001 <halimqolbun@gmail.com>
2026-09-12 11:53:51 +0700

    fix blade compiled view path

---
+++ config/view.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'paths' => [
+        resource_path('views'),
+    ],
+
+    'compiled' => env(
+        'VIEW_COMPILED_PATH',
+        storage_path('framework/views'),
+    ),
+
+    'relative_hash' => false,
+
+    'cache' => true,
+
+    'compiled_extension' => 'php',
+
+    'check_cache_timestamps' => true,
+
+];
