4cf3407f71c81d1e7acbf99fd8b5df95ed4413f6
Byeone001 <halimqolbun@gmail.com>
2026-09-16 00:16:48 +0700

    Konfigurasi Reset Password

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,11 +12,6 @@ $app = Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-   /* ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'admin' => AdminMiddleware::class,
-        ]);
-    })*/
 
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
