df7f1a57cced873a7e7b5965e21a3b90947a8f72
Byeone001 <halimqolbun@gmail.com>
2026-09-15 22:18:35 +0700

    Konfigurasi UI/UX 1

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -22,7 +22,10 @@
   <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
   <!-- Vite CSS & JS -->
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+
+ <!-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> -->
+ <!-- <script src="{{ mix('js/app.js') }}"></script> -->
 
   @stack('styles')
 </head>
