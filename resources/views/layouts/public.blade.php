cdd95943e19aa134197d5ea81d334267bfd141dc
Byeone001 <halimqolbun@gmail.com>
2026-09-15 22:33:07 +0700

    Konfigurasi Proxy 1

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -21,12 +21,6 @@
   <!-- Custom Theme CSS -->
   <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
-  <!-- Vite CSS & JS -->
-  <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
-
- <!-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> -->
- <!-- <script src="{{ mix('js/app.js') }}"></script> -->
-
   @stack('styles')
 </head>
 <body>
